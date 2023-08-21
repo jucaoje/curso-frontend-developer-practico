a1733bc147489931fb5c965a5e083163678f00ab
jucaoje <jucaoje@gmail.com>
2023-08-21 17:37:54 -0400

    fusion de detalle de prodcutos desde js a html

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const menuMobile = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside =document.querySelector('.product-detail');
-
-//esta es la forma corta se remplaza el nombre de la funcion parentesis                                               se coloca  la   la flecha y dentro de las llaves se coloca lo que va hacer la funcion, la de abajo es lo forma convencional.
+const cardContainer = document.querySelector('.cards-container');
+//esta es la forma corta se remplaza el nombre de la funcion parentesis                               se coloca  la   la flecha y dentro de las llaves se coloca lo que va hacer la funcion, la de abajo es lo forma convencional.
 menuEmail.addEventListener('click', ()=> {   
     const isAsideClosed = aside.classList.contains('inactive');
 
@@ -33,10 +33,89 @@ menuCarritoIcon.addEventListener('click', ()=> {
     aside.classList.toggle('inactive');
 })
 
+const productList = [];
+productList.push({
+  name: 'Bike',
+  price:  120,
+  image:  'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+  name: 'Clock',
+  price:  50,
+  image:  'https://picsum.photos/640/640?r=4851',
+});
+productList.push({
+  name: 'Kitchen',
+  price:  230,
+  image:  'https://picsum.photos/640/640?r=2483',
+});
+productList.push({
+  name: 'Bike',
+  price:  120,
+  image:  'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+  name: 'Clock',
+  price:  50,
+  image:  'https://picsum.photos/640/640?r=4851',
+});
+productList.push({
+  name: 'Kitchen',
+  price:  230,
+  image:  'https://picsum.photos/640/640?r=2483',
+});
+productList.push({
+  name: 'Clock',
+  price:  50,
+  image:  'https://picsum.photos/640/640?r=4851',
+});
+productList.push({
+  name: 'Kitchen',
+  price:  230,
+  image:  'https://picsum.photos/640/640?r=2483',
+});
 
-/*menuEmail.addEventListener('click', toggleDesktopMenu);
-function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
-}*/
 
-
+function renderProducts(arr){
+  for (product of productList){
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+  
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+  
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+  
+    const productInfoDiv = document.createElement ('div');
+  
+    const productPrice = document.createElement ('p');
+    productPrice.innerText = '$' + product.price;
+  
+    const productName = document.createElement ('p');
+    productName.innerText =  product.name;
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+    const productInfoFigure =document.createElement ('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardContainer.appendChild(productCard);
+  
+  
+  
+  
+  
+  }
+}
+renderProducts(productList);
